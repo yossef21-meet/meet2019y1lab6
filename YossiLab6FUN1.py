ba425885e73fa2425e7017ebf82dfaf22ca3af7a
yossef21-meet <yossef21@meet.mit.edu>
2019-07-28 13:58:37 +0300

    Stopping here

---
+++ YossiLab6FUN1.py
@@ -0,0 +1,27 @@
+def add_numbers():
+    total = 0
+    for number in range(1, 10 + 1):
+        return(number)
+        total = total + number
+    return(total)
+answer = add_numbers()
+print(answer)
+
+print(
+
+
+
+
+    )
+def add_number(start,end):
+    total = 0
+    for number in range(start, end):
+        
+        total = total + number
+        return total
+answer = add_number(333,777)
+print(answer)
+
+
+
+
